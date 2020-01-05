479357349811cf4d1731bc45b5349f19f5de9fb5
JaFaR <sysadjafarpr@gmail.com>
2020-01-05 10:16:08 +0300

    initial version

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_cashback(t *testing.T) {
+
+	tests := []struct {
+		name string
+		amount int
+		want int
+	}{
+		// TODO: Add test cases.
+		{"Have cashback", 5000,250},
+		{"No cashback",  1000,250},
+		{"Cashback on bound", 3000,150},
+	}
+	for _, tt := range tests {
+		got :=cashback(test.amount)
+		if got !=test.want {
+			t.Error("for cashback test:", test.name,"got:",got, "want:",test.want)
+
+		}
+	}
+}
